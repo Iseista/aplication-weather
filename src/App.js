b7ef5da9d1db82ffdbaba36c885346a5b89d1a2e
Jerson Quintana <jersonscaner@gmail.com>
2022-05-11 16:35:56 -0500

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,76 +1,81 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+
 
 
 function App() {
 
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=7ce6f05e8f9e52dac8156b2d0cfbdbcf`
+  const [weather, setWeather] = useState({})
+  
+  useEffect(()=>{
 
-  const getWeather = async (location) =>{
+     //Geolocalizacion
+  function success(pos) {
+    let crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('More or less ' + crd.accuracy + ' meters.');
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=7ce6f05e8f9e52dac8156b2d0cfbdbcf`)
+      .then(res => setWeather(res.data))
 
-  }
 
-  const miIp = (location) =>{
-    const {latitude, longitude} = location.coords
-  }
- 
-  const searchLocation = (event)=>{
-    if(event.key === "Enter"){
-      axios.get(url).then((response)=>{
-        setData(response.data)
-        console.log(response.data)
-      })
-      setLocation('')
-    }
-  }
+       
+  };
+  
+  function error(err) {
+    console.log('El usuario no permitió la localización');
+  };
+  
+  navigator.geolocation.getCurrentPosition(success, error);
+  //Fin geolocalizacion
+  },[])
+
+
   return (
     <div className="App">
-      <div className='search'>
-        <input value={location} onChange={event => setLocation (event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder=" Enter Location" type="text">
-        </input>
-        <i class='bx bxs-location-plus'></i>
-      </div>
+    
       <div className="article">
-        <h1>{data.name}</h1>
-        {data.main ?<p className="grados">{data.main.temp.toFixed()}°F</p>:null}
-        <i class='bx bxs-chevron-right'></i>
-        {data.weather ?<p className="clima">{data.weather[0].main}</p>:null}
+        <h1>{weather.name}</h1>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} atl=""/>
+        
+        {weather.main ?<p className="grados">{(weather.main.temp-273.15).toFixed(1)}°C</p>:null}
+        {weather.weather ?<p className="clima">{weather.weather[0].main}</p>:null}
       </div>
 
-    {data.name != undefined &&
       <div className="section">
-        <div className="caja">
-        <i class='bx bx-wind'></i>
+         <div className="caja">
+          <i class='bx bx-wind'></i>
           <h2>Wind:</h2>
-          {data.wind ?<h3>{data.wind.speed} MPH</h3>:null}
-        </div>
+          {weather.wind ?<h3>{weather.wind.speed} MPH</h3>:null}
+         </div>
 
-        <div className="caja">
-        <i class='bx bx-cloud'></i>
+         <div className="caja">
+          <i class='bx bx-cloud'></i>
           <h2>Clouds:</h2>
-          <h3>40%</h3>
-        </div>
+          {weather.weather ?<h3>{weather.weather[0].main}</h3>:null}
+         </div>
 
-        <div className="caja">
-        <i class='bx bxs-thermometer' ></i>
+         <div className="caja">
+          <i class='bx bxs-thermometer' ></i>
           <h2>Preesure:</h2>
-          {data.main ?<h3>{data.main.pressure}</h3>:null}
-        </div>
+          {weather.main ?<h3>{weather.main.pressure}</h3>:null}
+         </div>
 
-        <div className="caja">
-        <i class='bx bx-water'></i>
+         <div className="caja">
+          <i class='bx bx-water'></i>
           <h2>Humidity:</h2>
-          {data.main ?<h3>{data.main.humidity}%</h3>:null}
-        </div>
+          {weather.main ?<h3>{weather.main.humidity}%</h3>:null}
+         </div>
+
       </div>
-    }
+
     </div>
+
   );
+
 }
 
 export default App;
